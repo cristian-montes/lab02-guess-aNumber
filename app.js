e75531d26c78c1a78922adef4eba6dd53b05af80
Cristian Montes <cmontesmujica@gmail.com>
2021-06-30 15:13:18 -0700

    added working event listener with working TL, TH and YW and counts left messages

--- app.js
+++ app.js
@@ -6,3 +6,29 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const resultArea = document.getElementById('result-area');
+const guessInput = document.getElementById('guess-input');
+const submitBtn = document.getElementById('submit-btn');
+const guessArea = document.getElementById('guess-area');
+
+const targetNumber = Math.floor(Math.random() * 20);
+let count = 4;
+
+
+// EVENT LISTNER
+submitBtn.addEventListener('click', ()=> {
+    
+    count --;
+    
+    if (Number(guessInput.value) < targetNumber){
+        resultArea.textContent = 'TOO LOW';
+    } if (Number(guessInput.value) > targetNumber){
+        resultArea.textContent = 'TOO HIGH';
+    } if (Number(guessInput.value) === targetNumber){
+        resultArea.textContent = 'YOU WIN';
+    }
+   
+    guessArea.textContent = `You have ${count} left`;
+});
+
+
