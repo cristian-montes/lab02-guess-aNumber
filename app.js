32d51e13dfe053adc0058612c6f99b7d919d76c7
Cristian Montes <cmontesmujica@gmail.com>
2021-06-30 19:01:26 -0700

    added reset button

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ const resultArea = document.getElementById('result-area');
 const guessInput = document.getElementById('guess-input');
 const submitBtn = document.getElementById('submit-btn');
 const guessArea = document.getElementById('guess-area');
+const restBtn = document.getElementById('reset-btn'); 
 
 //MODIFED CONSTS'
 const targetNumber = Math.floor(Math.random() * 20);
 
 let count = 4;
 
+
+// EVENT LISTENER - RESET BTN 
+restBtn.addEventListener('click', ()=> {
+    count = 4;
+    guessInput.value = '';
+    resultArea.textContent = '';  
+    document.getElementById('submit-btn').disabled = false;
+    guessArea.textContent = `You have ${count} oportunities AGAIN!`;
+   
+});
 
 // EVENT LISTNER - SUBMIT BTN
 submitBtn.addEventListener('click', ()=> {
